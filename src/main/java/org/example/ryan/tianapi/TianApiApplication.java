698d98562df1b24ce12254f39df187bc96b65ab2
ryan <ryan@dynadot.com>
2025-12-11 13:07:37 +0800

    feat: initial project

---
+++ src/main/java/org/example/ryan/tianapi/TianApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.ryan.tianapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TianApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TianApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ryan/tianapi/TianApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ryan.tianapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TianApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
